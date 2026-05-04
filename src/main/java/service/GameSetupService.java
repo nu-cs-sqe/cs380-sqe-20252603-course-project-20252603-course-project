2b12677d330bd8a1954d30ae12190eaa42ec09ae
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-04 18:08:59 -0500

    fixed format to align to other teammates file storage and work
    # Conflicts:
    #	src/main/java/service/GameSetupService.java
    #	src/test/java/service/GameSetupServiceTest.java

--- src/main/java/service/GameSetupService.java
+++ src/main/java/service/GameSetupService.java
@@ -1,30 +1,16 @@
 package service;
 
+import model.GamePhase;
+import model.GameState;
+import model.Player;
 
 import java.util.List;
-import model.Player;
-import model.Territory;
-import model.GameState;
-import model.GamePhase;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-
-import java.util.Set;
-
-import static domain.GameConstants.TOTAL_TERRITORIES;
-
-public class GameSetupService{
+public class GameSetupService {
 
     public static final int MIN_PLAYERS = 2;
     public static final int MAX_PLAYERS = 6;
-    private List<Player> Players = new ArrayList<>();
 
-    // void initializeTurnOrder(GameState gameState)
-    public void initializeTurnOrder(GameState gameState){
-        List<Player> players_TO = gameState.getPlayers();
-        // Collections.shuffle(players_TO);
-        gameState.setTurnOrder(players_TO);
     public void validatePlayerCount(int playerCount) {
         if (playerCount < MIN_PLAYERS || playerCount > MAX_PLAYERS){
             throw new IllegalArgumentException(
@@ -33,15 +19,17 @@ public class GameSetupService{
         }
     }
 
-    // void startFirstTurn(GameState gameState)
+    public void initializeTurnOrder(GameState gameState){
+        List<Player> players_TO = gameState.getPlayers();
+        // Collections.shuffle(players_TO);
+        gameState.setTurnOrder(players_TO);
+    }
+
     public void startFirstTurn(GameState gameState){
         List<Player> players_TO = gameState.getTurnOrder();
         Player first_pl = players_TO.get(0);
         gameState.setCurrentPlayer(first_pl);
     }
-
-    // GameState createNewGame(List<String> names, List<PlayerColor> colors)
-
     public GameState createNewGame(List<String> names, List<String> colors){
         // create a new game state
         GameState gameState = new GameState();
@@ -49,21 +37,18 @@ public class GameSetupService{
         // call player generation
         // call territory generation
         // call territory distribution
-    public List<Player> getPlayers(){
-        return Players;
+
+        return gameState;
+    }
+    public GameState orchestration(List<String> pre_names, List<String> pre_colors){
+        GameState gameState = createNewGame(pre_names, pre_colors);
+        initializeTurnOrder(gameState);
+        startFirstTurn(gameState);
+        gameState.setCurrentPhase(GamePhase.REINFORCEMENT);
+        return gameState;
+
+    }
     }
 
-    public void createPlayers(List<String> names, List<PlayerColor> colors) {
-        if (names.size() != colors.size()){
-            throw new IllegalArgumentException("Size of name list (" + names.size() + ") and color list (" + colors.size() + ") differ.");
-        }
-        int numberOfPlayers = names.size();
-        validatePlayerCount(numberOfPlayers);
-        validateUniqueColors(colors);
-        Players.clear();
-        for(int i = 0; i < numberOfPlayers; i++){
-            Players.add(new Player(i, names.get(i), colors.get(i), 0, new ArrayList<>()));
-        }
-    }
 
-}
+

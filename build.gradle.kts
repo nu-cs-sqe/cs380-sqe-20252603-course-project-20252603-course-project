d957777a8daffc8b926f8f3bd48f55e88ad197ff
rj2373 <talliemolano2027@u.northwestern.edu>
2026-05-07 18:26:36 -0500

    add jacoco code coverage setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    jacoco
 }
 
 group = "nu.csse.sqe"
@@ -26,4 +27,14 @@ tasks.compileJava {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = false
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("reports/jacoco")
+    }
 }

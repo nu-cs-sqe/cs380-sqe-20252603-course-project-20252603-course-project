ab04a4f08403f8c59147810d0be6bcedce3765c7
Reed Gunn <reedmgunn@gmail.com>
2026-05-07 14:49:28 -0500

    test: add failing TDD tests for Location based on BVA

---
+++ src/main/java/domain/Location.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Location {
+    private final int x;
+    private final int y;
+
+    public Location(int x, int y) {
+        this.x = 0; // skeleton — not yet storing parameter
+        this.y = 0; // skeleton — not yet storing parameter
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}

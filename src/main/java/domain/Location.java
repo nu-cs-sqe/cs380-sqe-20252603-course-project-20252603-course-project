ff02c8de53b17760f90c25d3b13d1c377e3c5cf1
rj2373 <talliemolano2027@u.northwestern.edu>
2026-05-07 15:07:52 -0500

    add board and location placeholders

---
+++ src/main/java/domain/Location.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Location {
+    public Location(int x, int y) {
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+    public int getY() {
+        return 0;
+    }
+}

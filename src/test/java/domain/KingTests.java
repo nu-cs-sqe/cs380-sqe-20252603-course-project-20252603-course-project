c4af2802478e8a3881559c86d4e6b076535f3e31
rj2373 <talliemolano2027@u.northwestern.edu>
2026-05-07 21:27:21 -0500

    feat(green): pass King test case 13

--- src/test/java/domain/KingTests.java
+++ src/test/java/domain/KingTests.java
@@ -94,4 +94,11 @@ public class KingTests {
 
         assertTrue(king.isValidMoveShape(new Location(4, 4), new Location(3, 4)));
     }
+
+    @Test
+    public void IsValidMoveShape_OneSquareDiagonal_ReturnsTrue() {
+        King king = new King(PieceColor.WHITE);
+
+        assertTrue(king.isValidMoveShape(new Location(4, 4), new Location(5, 5)));
+    }
 }

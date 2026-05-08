b41526d557d97d6447e3686881e11001601d15cb
rj2373 <talliemolano2027@u.northwestern.edu>
2026-05-07 20:27:25 -0500

    feat(green): pass Pawn test case 10

--- src/main/java/domain/piece/Pawn.java
+++ src/main/java/domain/piece/Pawn.java
@@ -19,6 +19,9 @@ public class Pawn extends Piece {
         int dx = to.getX() - from.getX();
         int dy = to.getY() - from.getY();
 
+        if (getColor() == PieceColor.WHITE && dx == 0 && from.getY() == 6 && dy == -1) {
+            return true;
+        }
         return (getColor() == PieceColor.WHITE && dx == 0 && from.getY() == 6 && dy == -2)
                 || (getColor() == PieceColor.BLACK && dx == 0 && from.getY() == 1 && dy == 2);
     }

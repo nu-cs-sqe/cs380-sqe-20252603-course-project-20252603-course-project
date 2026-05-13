d498d36a6c992d4660f153c70f496fb9fb193216
Trenton Kim <trentonkim2027@u.northwestern.edu>
2026-05-13 16:09:04 -0500

    added support for gsv to have 4 components

--- src/main/java/ui/GameStatsView.java
+++ src/main/java/ui/GameStatsView.java
@@ -5,5 +5,15 @@ import javax.swing.*;
 public class GameStatsView extends JPanel {
     public GameStatsView(String player1Name, String player2Name) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JLabel a = new JLabel();
+        JLabel b = new JLabel();
+        JLabel c = new JLabel();
+        JLabel d = new JLabel();
+
+        add(a);
+        add(b);
+        add(c);
+        add(d);
     }
 }

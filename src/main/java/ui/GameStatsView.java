6a8dc9ee1ef943d408ca26d252413ff007963be6
Trenton Kim <trentonkim2027@u.northwestern.edu>
2026-05-13 16:58:35 -0500

    verified player 1 (alice, white)

--- src/main/java/ui/GameStatsView.java
+++ src/main/java/ui/GameStatsView.java
@@ -6,14 +6,21 @@ public class GameStatsView extends JPanel {
     public GameStatsView(String player1Name, String player2Name) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        JLabel a = new JLabel();
-        JLabel b = new JLabel();
-        JLabel c = new JLabel();
-        JLabel d = new JLabel();
+        JLabel playerInfoLabel =
+                new JLabel("Player Information");
 
-        add(a);
-        add(b);
-        add(c);
-        add(d);
+        JLabel player1Label =
+                new JLabel("Player 1: " + player1Name + " (Team: White)");
+
+        JLabel player2Label =
+                new JLabel("Player 2: " + player2Name + " (Team: Black)");
+
+        JLabel currentPlayerLabel =
+                new JLabel("Current Player: " + player1Name);
+
+        add(playerInfoLabel);
+        add(player1Label);
+        add(player2Label);
+        add(currentPlayerLabel);
     }
 }

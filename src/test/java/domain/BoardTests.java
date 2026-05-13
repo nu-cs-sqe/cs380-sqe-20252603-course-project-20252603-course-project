10228eea7003783669a7b0eeb2c5489ad7977cc9
Trenton Kim <trentonkim2027@u.northwestern.edu>
2026-05-13 15:10:18 -0500

    added support for test 1/22, added test 2/22 for board setup

--- src/test/java/domain/BoardTests.java
+++ src/test/java/domain/BoardTests.java
@@ -1,5 +1,6 @@
 package domain;
 
+import domain.piece.Piece;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,5 +12,15 @@ public class BoardTests {
         Board board = new Board();
 
         assertNotNull(board);
+    }
+
+    @Test
+    void snapshotHasCorrectDimensions() {
+        Board board = new Board();
+
+        Piece[][] snapshot = board.getSnapshot();
+
+        assertEquals(8, snapshot.length);
+        assertEquals(8, snapshot[0].length);
     }
 }

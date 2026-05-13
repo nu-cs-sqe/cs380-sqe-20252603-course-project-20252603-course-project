e6525e9d2ded8da79a207f065935ea0e40f29865
Trenton Kim <trentonkim2027@u.northwestern.edu>
2026-05-13 17:13:33 -0500

    added first test for main (no crash)

--- src/test/java/ui/MainTests.java
+++ src/test/java/ui/MainTests.java
@@ -1,4 +1,11 @@
 package ui;
 
+import com.sun.tools.javac.Main;
+import org.junit.jupiter.api.Test;
+
 public class MainTests {
+    @Test
+    void mainRunsWithoutCrash() {
+        Main.main(new String[]{});
+    }
 }

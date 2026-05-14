3285d7439459ed7963bca185800b94209895c5d7
rj2373 <talliemolano2027@u.northwestern.edu>
2026-05-14 17:04:33 -0500

    feat(green): implement GameStatsView.updateCurrentPlayerLabel

--- src/main/java/ui/GameStatsView.java
+++ src/main/java/ui/GameStatsView.java
@@ -1,7 +1,17 @@
 package ui;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GameStatsView extends JPanel {
-    public GameStatsView(String player1Name, String player2Name) {}
+
+    public JLabel currentPlayerLabel;
+
+    public GameStatsView(String player1Name, String player2Name) {
+        currentPlayerLabel = new JLabel("Current Player: " + player1Name);
+    }
+
+    public void updateCurrentPlayerLabel(String playerName) {
+        currentPlayerLabel.setText("Current Player: " + playerName);
+    }
 }

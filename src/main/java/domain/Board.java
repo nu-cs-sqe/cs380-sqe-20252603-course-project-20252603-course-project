ba34d14d0479723386ea8b22c5998d8fef721645
rj2373 <talliemolano2027@u.northwestern.edu>
2026-05-14 16:46:15 -0500

    feat(green): pass Board test case 23

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -3,7 +3,22 @@ package domain;
 import domain.piece.Piece;
 
 public class Board {
+
+    Piece[][] pieces;
+    GameState currentGameState;
+    Location whiteKingLocation;
+    Location blackKingLocation;
+
+    public Board() {
+        pieces = new Piece[8][8];
+        currentGameState = GameState.WHITE_TURN;
+    }
+
     public Piece[][] getSnapshot() {
         return null;
+    }
+
+    public GameState getCurrentGameState() {
+        return currentGameState;
     }
 }

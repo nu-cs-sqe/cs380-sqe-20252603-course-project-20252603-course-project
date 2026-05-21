50bfeb50a6efdf3d65cf84e47b868d34db620c5d
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-21 07:17:20 -0500

    Initial code for canFortify

--- src/main/java/service/FortifyService.java
+++ src/main/java/service/FortifyService.java
@@ -1,4 +1,29 @@
 package service;
 
+import model.Player;
+import model.Territory;
+import model.GameState;
+
+
 public class FortifyService {
+
+
+    public boolean canFortify(Player player, Territory source, Territory destination, int armiesToMove, GameState gameState){
+        if (source.getArmyCount() < armiesToMove){
+            return false;
+        }
+        if (source.getOwner().equals(player)){
+            return false;
+        }
+        if (destination.getOwner().equals(player)){
+            return false;
+        }
+        return true;
+    }
+    public boolean areConnectedThroughOwnedTerritories(Player player, Territory source, Territory destination, GameState gameState){
+
+        return true;
+    }
+
+
 }

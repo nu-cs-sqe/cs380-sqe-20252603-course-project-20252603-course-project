79a04132707d9097c650ec0b287bb0382327f105
doc4science <doc4science@gmail.com>
2026-05-26 10:22:01 -0500

    add 2nd test for canAttack

--- docs/bva/AttackService.md
+++ docs/bva/AttackService.md
@@ -1,6 +1,7 @@
 # Game Setup BVA
 
 ### Method under test: canAttack
-|                                                                          | State of the System                                                                                             | Expected output     | Implemented?         |
-|--------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------|---------------------|----------------------|
-| T1_shouldAllowAttackWhenTerritoriesAreAdjacentAndOwnedByDifferentPlayers | Two adjacent territories owned by different players where the attacker has more than 1 army on their territory. | Valid Attack (True) | :white_check_mark:   |
+|                                                                          | State of the System                                                                                             | Expected output       | Implemented?         |
+|--------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------|-----------------------|----------------------|
+| T1_shouldAllowAttackWhenTerritoriesAreAdjacentAndOwnedByDifferentPlayers | Two adjacent territories owned by different players where the attacker has more than 1 army on their territory. | Valid Attack (True)   | :white_check_mark:   |
+| T2_shouldRejectAttackWhenAttackingTerritoryIsNotOwnedByCurrentPlayer     | Attacking territory is owned by a player other than the attacker. Otherwise normal state.                       | Failed Attack (False) | :white_check_mark:   |
--- src/test/java/service/AttackServiceTest.java
+++ src/test/java/service/AttackServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AttackServiceTest {
@@ -23,10 +24,30 @@ public class AttackServiceTest {
         Player defender = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
         Territory atackingTerritory = new Territory("Western United States", attacker, 5, Continent.NORTH_AMERICA);
         Territory defendingTerritory = new Territory("Eastern United States", defender, 1, Continent.NORTH_AMERICA);
-        controlled_territories2.add(atackingTerritory);
-        controlled_territories1.add(defendingTerritory);
+        controlled_territories1.add(atackingTerritory);
+        controlled_territories2.add(defendingTerritory);
         attacker.setControlledTerritories(controlled_territories1);
         defender.setControlledTerritories(controlled_territories2);
         assertTrue(attackService.canAttack(attacker, atackingTerritory, defendingTerritory));
+    }
+
+    @Test
+    void T2_shouldRejectAttackWhenAttackingTerritoryIsNotOwnedByCurrentPlayer(){
+        TerritoryAdjacencyService adjacencyService = new TerritoryAdjacencyService();
+        AttackService attackService = new AttackService(adjacencyService);
+        List<Territory> controlled_territories1 = new ArrayList<>();
+        List<Territory> controlled_territories2 = new ArrayList<>();
+        List<Territory> controlled_territories3 = new ArrayList<>();
+        Player attacker = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
+        Player defender = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
+        Player otherPlayer = new Player(3, "C", PlayerColor.GREEN, 1, controlled_territories3);
+        Territory atackingTerritory = new Territory("Western United States", otherPlayer, 5, Continent.NORTH_AMERICA);
+        Territory defendingTerritory = new Territory("Eastern United States", defender, 1, Continent.NORTH_AMERICA);
+        controlled_territories2.add(defendingTerritory);
+        controlled_territories3.add(atackingTerritory);
+        attacker.setControlledTerritories(controlled_territories1);
+        defender.setControlledTerritories(controlled_territories2);
+        otherPlayer.setControlledTerritories(controlled_territories3);
+        assertFalse(attackService.canAttack(attacker, atackingTerritory, defendingTerritory));
     }
 }

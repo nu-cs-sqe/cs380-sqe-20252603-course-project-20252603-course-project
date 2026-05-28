fa131fc9ea97e9632464b1860188280aae5159f4
Connor King <114192878+Cking197@users.noreply.github.com>
2026-05-28 00:07:55 -0500

    fortifyService tests pass checkstyle

--- src/test/java/service/FortifyServiceTest.java
+++ src/test/java/service/FortifyServiceTest.java
@@ -16,97 +16,239 @@ public class FortifyServiceTest {
 
     @Test
     void T1_shouldAllowFortifyBetweenConnectedOwnedTerritories() {
-        FortifyService FS = new FortifyService();
+        FortifyService fortifyService = new FortifyService();
         GameState gameState = new GameState();
-        List<Territory> controlled_territories = new ArrayList<>();
-        Player player1 = new Player(1, "A", PlayerColor.RED, 1, controlled_territories);
-        Territory t1 = new Territory("Western United States", player1, 5, Continent.NORTH_AMERICA);
-        Territory t2 = new Territory("Eastern United States", player1, 1, Continent.NORTH_AMERICA);
-        controlled_territories.add(t1);
-        controlled_territories.add(t2);
-        player1.setControlledTerritories(controlled_territories);
-        assertTrue(FS.areConnectedThroughOwnedTerritories(player1, t1, t2, gameState));
-        assertTrue(FS.canFortify(player1, t1, t2, 2, gameState));
+        List<Territory> controlledTerritories = new ArrayList<>();
+        Player player1 = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories
+        );
+        Territory t1 = new Territory(
+                "Western United States",
+                player1,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory t2 = new Territory(
+                "Eastern United States",
+                player1,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories.add(t1);
+        controlledTerritories.add(t2);
+        player1.setControlledTerritories(controlledTerritories);
+        assertTrue(
+                fortifyService.areConnectedThroughOwnedTerritories(
+                        player1,
+                        t1,
+                        t2,
+                        gameState
+                )
+        );
+        assertTrue(fortifyService.canFortify(player1, t1, t2, 2, gameState));
     }
 
     @Test
     void T2_shouldRejectFortifyWhenSourceIsNotOwnedByPlayer() {
-        FortifyService FS = new FortifyService();
+        FortifyService fortifyService = new FortifyService();
         GameState gameState = new GameState();
-        List<Territory> controlled_territories1 = new ArrayList<>();
-        List<Territory> controlled_territories2 = new ArrayList<>();
-        Player player1 = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
-        Player player2 = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
-        Territory t1 = new Territory("Western United States", player2, 5, Continent.NORTH_AMERICA);
-        Territory t2 = new Territory("Eastern United States", player1, 1, Continent.NORTH_AMERICA);
-        controlled_territories2.add(t1);
-        controlled_territories1.add(t2);
-        player1.setControlledTerritories(controlled_territories1);
-        player2.setControlledTerritories(controlled_territories2);
-        assertTrue(FS.areConnectedThroughOwnedTerritories(player1, t1, t2, gameState));
-        assertFalse(FS.canFortify(player1, t1, t2, 2, gameState));
+        List<Territory> controlledTerritories1 = new ArrayList<>();
+        List<Territory> controlledTerritories2 = new ArrayList<>();
+        Player player1 = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories1
+        );
+        Player player2 = new Player(
+                2,
+                "B",
+                PlayerColor.BLUE,
+                1,
+                controlledTerritories2
+        );
+        Territory t1 = new Territory(
+                "Western United States",
+                player2,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory t2 = new Territory(
+                "Eastern United States",
+                player1,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories2.add(t1);
+        controlledTerritories1.add(t2);
+        player1.setControlledTerritories(controlledTerritories1);
+        player2.setControlledTerritories(controlledTerritories2);
+        assertTrue(
+                fortifyService.areConnectedThroughOwnedTerritories(
+                        player1,
+                        t1,
+                        t2,
+                        gameState
+                )
+        );
+        assertFalse(fortifyService.canFortify(player1, t1, t2, 2, gameState));
     }
 
     @Test
     void T3_shouldRejectFortifyWhenDestinationIsNotOwnedByPlayer() {
-        FortifyService FS = new FortifyService();
+        FortifyService fortifyService = new FortifyService();
         GameState gameState = new GameState();
-        List<Territory> controlled_territories1 = new ArrayList<>();
-        List<Territory> controlled_territories2 = new ArrayList<>();
-        Player player1 = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
-        Player player2 = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
-        Territory t1 = new Territory("Western United States", player1, 5, Continent.NORTH_AMERICA);
-        Territory t2 = new Territory("Eastern United States", player2, 1, Continent.NORTH_AMERICA);
-        controlled_territories1.add(t1);
-        controlled_territories2.add(t2);
-        player1.setControlledTerritories(controlled_territories1);
-        player2.setControlledTerritories(controlled_territories2);
-        assertTrue(FS.areConnectedThroughOwnedTerritories(player1, t1, t2, gameState));
-        assertFalse(FS.canFortify(player1, t1, t2, 2, gameState));
+        List<Territory> controlledTerritories1 = new ArrayList<>();
+        List<Territory> controlledTerritories2 = new ArrayList<>();
+        Player player1 = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories1
+        );
+        Player player2 = new Player(
+                2,
+                "B",
+                PlayerColor.BLUE,
+                1,
+                controlledTerritories2
+        );
+        Territory t1 = new Territory(
+                "Western United States",
+                player1,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory t2 = new Territory(
+                "Eastern United States",
+                player2,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories1.add(t1);
+        controlledTerritories2.add(t2);
+        player1.setControlledTerritories(controlledTerritories1);
+        player2.setControlledTerritories(controlledTerritories2);
+        assertTrue(
+                fortifyService.areConnectedThroughOwnedTerritories(
+                        player1,
+                        t1,
+                        t2,
+                        gameState
+                )
+        );
+        assertFalse(fortifyService.canFortify(player1, t1, t2, 2, gameState));
     }
 
     @Test
     void T4_shouldRejectFortifyWhenTerritoriesAreNotConnected() {
-        FortifyService FS = new FortifyService();
+        FortifyService fortifyService = new FortifyService();
         GameState gameState = new GameState();
-        List<Territory> controlled_territories1 = new ArrayList<>();
-        List<Territory> controlled_territories2 = new ArrayList<>();
-        Player player1 = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
-        Player player2 = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
-        Territory t1 = new Territory("Western United States", player1, 5, Continent.NORTH_AMERICA);
-        Territory t2 = new Territory("Iceland", player2, 1, Continent.EUROPE);
-        controlled_territories1.add(t1);
-        controlled_territories2.add(t2);
-        player1.setControlledTerritories(controlled_territories1);
-        player2.setControlledTerritories(controlled_territories2);
-        assertFalse(FS.areConnectedThroughOwnedTerritories(player1, t1, t2, gameState));
+        List<Territory> controlledTerritories1 = new ArrayList<>();
+        List<Territory> controlledTerritories2 = new ArrayList<>();
+        Player player1 = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories1
+        );
+        Player player2 = new Player(
+                2,
+                "B",
+                PlayerColor.BLUE,
+                1,
+                controlledTerritories2
+        );
+        Territory t1 = new Territory(
+                "Western United States",
+                player1,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory t2 = new Territory(
+                "Iceland",
+                player2,
+                1,
+                Continent.EUROPE
+        );
+        controlledTerritories1.add(t1);
+        controlledTerritories2.add(t2);
+        player1.setControlledTerritories(controlledTerritories1);
+        player2.setControlledTerritories(controlledTerritories2);
+        assertFalse(
+                fortifyService.areConnectedThroughOwnedTerritories(
+                        player1,
+                        t1,
+                        t2,
+                        gameState
+                )
+        );
     }
 
     @Test
     void T5_shouldRejectFortifyWhenSourceHasOnlyOneArmy() {
-        FortifyService FS = new FortifyService();
+        FortifyService fortifyService = new FortifyService();
         GameState gameState = new GameState();
-        List<Territory> controlled_territories = new ArrayList<>();
-        Player player1 = new Player(1, "A", PlayerColor.RED, 1, controlled_territories);
-        Territory t1 = new Territory("Western United States", player1, 1, Continent.NORTH_AMERICA);
-        Territory t2 = new Territory("Eastern United States", player1, 0, Continent.NORTH_AMERICA);
-        controlled_territories.add(t1);
-        controlled_territories.add(t2);
-        player1.setControlledTerritories(controlled_territories);
-        assertFalse(FS.canFortify(player1, t1, t2, 1, gameState));
+        List<Territory> controlledTerritories = new ArrayList<>();
+        Player player1 = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories
+        );
+        Territory t1 = new Territory(
+                "Western United States",
+                player1,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        Territory t2 = new Territory(
+                "Eastern United States",
+                player1,
+                0,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories.add(t1);
+        controlledTerritories.add(t2);
+        player1.setControlledTerritories(controlledTerritories);
+        assertFalse(fortifyService.canFortify(player1, t1, t2, 1, gameState));
     }
 
     @Test
     void T6_shouldRejectFortifyWhenMoveWouldLeaveSourceEmpty() {
-        FortifyService FS = new FortifyService();
+        FortifyService fortifyService = new FortifyService();
         GameState gameState = new GameState();
-        List<Territory> controlled_territories = new ArrayList<>();
-        Player player1 = new Player(1, "A", PlayerColor.RED, 1, controlled_territories);
-        Territory t1 = new Territory("Western United States", player1, 5, Continent.NORTH_AMERICA);
-        Territory t2 = new Territory("Eastern United States", player1, 1, Continent.NORTH_AMERICA);
-        controlled_territories.add(t1);
-        controlled_territories.add(t2);
-        player1.setControlledTerritories(controlled_territories);
-        assertFalse(FS.canFortify(player1, t1, t2, 5, gameState));
+        List<Territory> controlledTerritories = new ArrayList<>();
+        Player player1 = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories
+        );
+        Territory t1 = new Territory(
+                "Western United States",
+                player1,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory t2 = new Territory(
+                "Eastern United States",
+                player1,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories.add(t1);
+        controlledTerritories.add(t2);
+        player1.setControlledTerritories(controlledTerritories);
+        assertFalse(fortifyService.canFortify(player1, t1, t2, 5, gameState));
     }
 }

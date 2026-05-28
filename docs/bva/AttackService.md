8f37e7f8b3f99ff68fd4579bff6cefacde44deea
Connor King <114192878+Cking197@users.noreply.github.com>
2026-05-27 23:52:56 -0500

    attackService passes checkstyle + bva name adjusted for 80 char rule

--- docs/bva/AttackService.md
+++ docs/bva/AttackService.md
@@ -1,10 +1,10 @@
 # Game Setup BVA
 
 ### Method under test: canAttack
-|                                                                          | State of the System                                                                                             | Expected output       | Implemented?         |
-|--------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------|-----------------------|----------------------|
-| T1_shouldAllowAttackWhenTerritoriesAreAdjacentAndOwnedByDifferentPlayers | Two adjacent territories owned by different players where the attacker has more than 1 army on their territory. | Valid Attack (True)   | :white_check_mark:   |
-| T2_shouldRejectAttackWhenAttackingTerritoryIsNotOwnedByCurrentPlayer     | Attacking territory is owned by a player other than the attacker. Otherwise normal state.                       | Failed Attack (False) | :white_check_mark:   |
-| T3_shouldRejectAttackWhenDefendingTerritoryIsOwnedByCurrentPlayer        | Defending territory is owned by the attacker. Otherwise normal state.                                           | Failed Attack (False) | :white_check_mark:   |
-| T4_shouldRejectAttackWhenTerritoriesAreNotAdjacent                       | Attacking and defending territories are not adjacent. Otherwise normal state.                                   | Failed Attack (False) | :white_check_mark:   |
-| T5_shouldRejectAttackWhenAttackingTerritoryHasOnlyOneArmy                | Attacking player has only one army available on the territory they are attacking from. Otherwise normal state.  | Failed Attack (False) | :white_check_mark:   |
+|                                                                        | State of the System                                                                                             | Expected output       | Implemented?         |
+|------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------|-----------------------|----------------------|
+| T1_shouldAllowAttackIfTerritoriesAreAdjacentAndOwnedByDifferentPlayers | Two adjacent territories owned by different players where the attacker has more than 1 army on their territory. | Valid Attack (True)   | :white_check_mark:   |
+| T2_shouldRejectAttackIfAttackingTerritoryIsNotOwnedByCurrentPlayer     | Attacking territory is owned by a player other than the attacker. Otherwise normal state.                       | Failed Attack (False) | :white_check_mark:   |
+| T3_shouldRejectAttackIfDefendingTerritoryIsOwnedByCurrentPlayer      | Defending territory is owned by the attacker. Otherwise normal state.                                           | Failed Attack (False) | :white_check_mark:   |
+| T4_shouldRejectAttackIfTerritoriesAreNotAdjacent                     | Attacking and defending territories are not adjacent. Otherwise normal state.                                   | Failed Attack (False) | :white_check_mark:   |
+| T5_shouldRejectAttackIfAttackingTerritoryHasOnlyOneArmy              | Attacking player has only one army available on the territory they are attacking from. Otherwise normal state.  | Failed Attack (False) | :white_check_mark:   |
--- src/test/java/service/AttackServiceTest.java
+++ src/test/java/service/AttackServiceTest.java
@@ -14,88 +14,223 @@ import model.Territory;
 public class AttackServiceTest {
 
     @Test
-    void T1_shouldAllowAttackWhenTerritoriesAreAdjacentAndOwnedByDifferentPlayers() {
-        TerritoryAdjacencyService adjacencyService = new TerritoryAdjacencyService();
+    void T1_shouldAllowAttackIfTerritoriesAreAdjacentAndOwnedByDifferentPlayers() {
+        TerritoryAdjacencyService adjacencyService =
+        new TerritoryAdjacencyService();
         AttackService attackService = new AttackService(adjacencyService);
-        List<Territory> controlled_territories1 = new ArrayList<>();
-        List<Territory> controlled_territories2 = new ArrayList<>();
-        Player attacker = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
-        Player defender = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
-        Territory atackingTerritory = new Territory("Western United States", attacker, 5, Continent.NORTH_AMERICA);
-        Territory defendingTerritory = new Territory("Eastern United States", defender, 1, Continent.NORTH_AMERICA);
-        controlled_territories1.add(atackingTerritory);
-        controlled_territories2.add(defendingTerritory);
-        attacker.setControlledTerritories(controlled_territories1);
-        defender.setControlledTerritories(controlled_territories2);
-        assertTrue(attackService.canAttack(attacker, atackingTerritory, defendingTerritory));
+        List<Territory> controlledTerritories1 = new ArrayList<>();
+        List<Territory> controlledTerritories2 = new ArrayList<>();
+        Player attacker = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories1
+        );
+        Player defender = new Player(
+                2,
+                "B",
+                PlayerColor.BLUE,
+                1,
+                controlledTerritories2
+        );
+        Territory atackingTerritory = new Territory(
+                "Western United States",
+                attacker,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory defendingTerritory = new Territory(
+                "Eastern United States",
+                defender,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories1.add(atackingTerritory);
+        controlledTerritories2.add(defendingTerritory);
+        attacker.setControlledTerritories(controlledTerritories1);
+        defender.setControlledTerritories(controlledTerritories2);
+        assertTrue(attackService.canAttack(
+                attacker,
+                atackingTerritory,
+                defendingTerritory)
+        );
     }
 
     @Test
-    void T2_shouldRejectAttackWhenAttackingTerritoryIsNotOwnedByCurrentPlayer() {
-        TerritoryAdjacencyService adjacencyService = new TerritoryAdjacencyService();
+    void T2_shouldRejectAttackIfAttackingTerritoryIsNotOwnedByCurrentPlayer() {
+        TerritoryAdjacencyService adjacencyService =
+        new TerritoryAdjacencyService();
         AttackService attackService = new AttackService(adjacencyService);
-        List<Territory> controlled_territories1 = new ArrayList<>();
-        List<Territory> controlled_territories2 = new ArrayList<>();
-        List<Territory> controlled_territories3 = new ArrayList<>();
-        Player attacker = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
-        Player defender = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
-        Player otherPlayer = new Player(3, "C", PlayerColor.GREEN, 1, controlled_territories3);
-        Territory atackingTerritory = new Territory("Western United States", otherPlayer, 5, Continent.NORTH_AMERICA);
-        Territory defendingTerritory = new Territory("Eastern United States", defender, 1, Continent.NORTH_AMERICA);
-        controlled_territories2.add(defendingTerritory);
-        controlled_territories3.add(atackingTerritory);
-        attacker.setControlledTerritories(controlled_territories1);
-        defender.setControlledTerritories(controlled_territories2);
-        otherPlayer.setControlledTerritories(controlled_territories3);
-        assertFalse(attackService.canAttack(attacker, atackingTerritory, defendingTerritory));
+        List<Territory> controlledTerritories1 = new ArrayList<>();
+        List<Territory> controlledTerritories2 = new ArrayList<>();
+        List<Territory> controlledTerritories3 = new ArrayList<>();
+        Player attacker = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories1
+        );
+        Player defender = new Player(
+                2,
+                "B",
+                PlayerColor.BLUE,
+                1,
+                controlledTerritories2
+        );
+        Player otherPlayer = new Player(
+                3,
+                "C",
+                PlayerColor.GREEN,
+                1,
+                controlledTerritories3
+        );
+        Territory atackingTerritory = new Territory(
+                "Western United States",
+                otherPlayer,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory defendingTerritory = new Territory(
+                "Eastern United States",
+                defender,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories2.add(defendingTerritory);
+        controlledTerritories3.add(atackingTerritory);
+        attacker.setControlledTerritories(controlledTerritories1);
+        defender.setControlledTerritories(controlledTerritories2);
+        otherPlayer.setControlledTerritories(controlledTerritories3);
+        assertFalse(attackService.canAttack(
+                attacker,
+                atackingTerritory,
+                defendingTerritory)
+        );
     }
 
     @Test
-    void T3_shouldRejectAttackWhenDefendingTerritoryIsOwnedByCurrentPlayer() {
-        TerritoryAdjacencyService adjacencyService = new TerritoryAdjacencyService();
+    void T3_shouldRejectAttackIfDefendingTerritoryIsOwnedByCurrentPlayer() {
+        TerritoryAdjacencyService adjacencyService =
+        new TerritoryAdjacencyService();
         AttackService attackService = new AttackService(adjacencyService);
-        List<Territory> controlled_territories1 = new ArrayList<>();
-        Player attacker = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
-        Territory atackingTerritory = new Territory("Western United States", attacker, 5, Continent.NORTH_AMERICA);
-        Territory defendingTerritory = new Territory("Eastern United States", attacker, 1, Continent.NORTH_AMERICA);
-        controlled_territories1.add(atackingTerritory);
-        controlled_territories1.add(defendingTerritory);
-        attacker.setControlledTerritories(controlled_territories1);
-        assertFalse(attackService.canAttack(attacker, atackingTerritory, defendingTerritory));
+        List<Territory> controlledTerritories1 = new ArrayList<>();
+        Player attacker = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories1
+        );
+        Territory atackingTerritory = new Territory(
+                "Western United States",
+                attacker,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory defendingTerritory = new Territory(
+                "Eastern United States",
+                attacker,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories1.add(atackingTerritory);
+        controlledTerritories1.add(defendingTerritory);
+        attacker.setControlledTerritories(controlledTerritories1);
+        assertFalse(attackService.canAttack(
+                attacker,
+                atackingTerritory,
+                defendingTerritory)
+        );
     }
 
     @Test
-    void T4_shouldRejectAttackWhenTerritoriesAreNotAdjacent() {
-        TerritoryAdjacencyService adjacencyService = new TerritoryAdjacencyService();
+    void T4_shouldRejectAttackIfTerritoriesAreNotAdjacent() {
+        TerritoryAdjacencyService adjacencyService =
+        new TerritoryAdjacencyService();
         AttackService attackService = new AttackService(adjacencyService);
-        List<Territory> controlled_territories1 = new ArrayList<>();
-        List<Territory> controlled_territories2 = new ArrayList<>();
-        Player attacker = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
-        Player defender = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
-        Territory atackingTerritory = new Territory("Alberta", attacker, 5, Continent.NORTH_AMERICA);
-        Territory defendingTerritory = new Territory("Eastern United States", defender, 1, Continent.NORTH_AMERICA);
-        controlled_territories1.add(atackingTerritory);
-        controlled_territories2.add(defendingTerritory);
-        attacker.setControlledTerritories(controlled_territories1);
-        defender.setControlledTerritories(controlled_territories2);
-        assertFalse(attackService.canAttack(attacker, atackingTerritory, defendingTerritory));
+        List<Territory> controlledTerritories1 = new ArrayList<>();
+        List<Territory> controlledTerritories2 = new ArrayList<>();
+        Player attacker = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories1
+        );
+        Player defender = new Player(
+                2,
+                "B",
+                PlayerColor.BLUE,
+                1,
+                controlledTerritories2
+        );
+        Territory atackingTerritory = new Territory(
+                "Alberta",
+                attacker,
+                5,
+                Continent.NORTH_AMERICA
+        );
+        Territory defendingTerritory = new Territory(
+                "Eastern United States",
+                defender,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories1.add(atackingTerritory);
+        controlledTerritories2.add(defendingTerritory);
+        attacker.setControlledTerritories(controlledTerritories1);
+        defender.setControlledTerritories(controlledTerritories2);
+        assertFalse(attackService.canAttack(
+                attacker, 
+                atackingTerritory, 
+                defendingTerritory
+        ));
     }
 
     @Test
-    void T5_shouldRejectAttackWhenAttackingTerritoryHasOnlyOneArmy() {
-        TerritoryAdjacencyService adjacencyService = new TerritoryAdjacencyService();
+    void T5_shouldRejectAttackIfAttackingTerritoryHasOnlyOneArmy() {
+        TerritoryAdjacencyService adjacencyService =
+        new TerritoryAdjacencyService();
         AttackService attackService = new AttackService(adjacencyService);
-        List<Territory> controlled_territories1 = new ArrayList<>();
-        List<Territory> controlled_territories2 = new ArrayList<>();
-        Player attacker = new Player(1, "A", PlayerColor.RED, 1, controlled_territories1);
-        Player defender = new Player(2, "B", PlayerColor.BLUE, 1, controlled_territories2);
-        Territory atackingTerritory = new Territory("Western United States", attacker, 1, Continent.NORTH_AMERICA);
-        Territory defendingTerritory = new Territory("Eastern United States", defender, 1, Continent.NORTH_AMERICA);
-        controlled_territories1.add(atackingTerritory);
-        controlled_territories2.add(defendingTerritory);
-        attacker.setControlledTerritories(controlled_territories1);
-        defender.setControlledTerritories(controlled_territories2);
-        assertFalse(attackService.canAttack(attacker, atackingTerritory, defendingTerritory));
+        List<Territory> controlledTerritories1 = new ArrayList<>();
+        List<Territory> controlledTerritories2 = new ArrayList<>();
+        Player attacker = new Player(
+                1,
+                "A",
+                PlayerColor.RED,
+                1,
+                controlledTerritories1
+        );
+        Player defender = new Player(
+                2,
+                "B",
+                PlayerColor.BLUE,
+                1,
+                controlledTerritories2
+        );
+        Territory atackingTerritory = new Territory(
+                "Western United States",
+                attacker,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        Territory defendingTerritory = new Territory(
+                "Eastern United States",
+                defender,
+                1,
+                Continent.NORTH_AMERICA
+        );
+        controlledTerritories1.add(atackingTerritory);
+        controlledTerritories2.add(defendingTerritory);
+        attacker.setControlledTerritories(controlledTerritories1);
+        defender.setControlledTerritories(controlledTerritories2);
+        assertFalse(attackService.canAttack(
+                attacker, 
+                atackingTerritory, 
+                defendingTerritory
+        ));
     }
 
 }

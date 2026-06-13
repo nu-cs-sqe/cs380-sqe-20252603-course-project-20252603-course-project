a89026ead82384f576c4598d8ede1300f1b097c3
Christopher Zhou <chriszhou2028@u.northwestern.edu>
2026-06-13 15:52:44 -0500

    executeCardAction_NextPlayerHasExactlyTwoSameCatCards_AddsOneTurn passed

--- src/test/java/domain/OneCatPolicyCardControllerTests.java
+++ src/test/java/domain/OneCatPolicyCardControllerTests.java
@@ -112,4 +112,40 @@ public class OneCatPolicyCardControllerTests {
 
         verify(mockView, mockGameController, mockGame, mockInitiator, mockNextPlayer, cat1, cat2, cat3, cat4);
     }
+
+    @Test
+    public void executeCardAction_NextPlayerHasExactlyTwoSameCatCards_AddsOneTurn() {
+        OneCatPolicyCardControllerView mockView = createStrictMock(OneCatPolicyCardControllerView.class);
+        GameController mockGameController = createMock(GameController.class);
+        Game mockGame = createMock(Game.class);
+        Player mockInitiator = createMock(Player.class);
+        Player mockNextPlayer = createMock(Player.class);
+
+        Card cat1A = createMock(Card.class);
+        Card cat1B = createMock(Card.class);
+
+        expect(cat1A.getType()).andReturn(CardType.CAT_CARD_1).anyTimes();
+        expect(cat1B.getType()).andReturn(CardType.CAT_CARD_1).anyTimes();
+
+        ArrayList<Card> nextPlayerHand = new ArrayList<>(java.util.Arrays.asList(cat1A, cat1B));
+
+        expect(mockGameController.getGame()).andReturn(mockGame);
+        expect(mockGameController.getNextPlayerIndex()).andReturn(1);
+
+        expect(mockGame.getAlivePlayers()).andReturn(java.util.Arrays.asList(mockInitiator, mockNextPlayer));
+
+        expect(mockNextPlayer.getHand()).andReturn(nextPlayerHand);
+
+        expect(mockGameController.getNextPlayerTurnsLeft()).andReturn(1);
+        mockGameController.setNextPlayerTurnsLeft(2);
+
+        mockView.displayTurnsAdded(1);
+
+        replay(mockView, mockGameController, mockGame, mockInitiator, mockNextPlayer, cat1A, cat1B);
+
+        OneCatPolicyCardController controller = new OneCatPolicyCardController(mockView);
+        controller.executeCardAction(mockGameController, mockInitiator, Optional.empty());
+
+        verify(mockView, mockGameController, mockGame, mockInitiator, mockNextPlayer, cat1A, cat1B);
+    }
 }
